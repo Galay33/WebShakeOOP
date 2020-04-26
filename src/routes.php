db1fac55200c2831b33a658606c3b1693add9195
Galay33 <galashov@gmail.com>
2020-04-26 19:28:55 +0500

    Commit 2

---
+++ src/routes.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
+    '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view'],
+    '~$~' => [\MyProject\Controllers\MainController::class, 'main'],
+
+];
